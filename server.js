4336254679275f1d9f9ce85ba144fecd26e7a0ba
dfuenning <dfuenning@gmail.com>
2020-06-26 15:50:14 -0400

    reverting heroku code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,16 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/budget";
-const options = {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
-};
-mongoose.connect(MONGODB_URI,options)
+  useFindAndModify: false
+});
 
-// routes
+// routes here
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
